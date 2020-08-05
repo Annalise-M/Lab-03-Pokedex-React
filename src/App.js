73e1861ce00605436e44404391ee495e8397ed6d
Annalise Murphy <nali247@gmail.com>
2020-08-05 15:04:06 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         I ADDED SOMETHING HERE FOR THE INITIAL COMMIT
         </a>
       </header>
     </div>
