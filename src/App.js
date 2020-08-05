3b1954c1b9e361e6bd2ed067c7183fb9e152f88c
Annalise Murphy <nali247@gmail.com>
2020-08-05 15:30:37 -0700

    style components added and linked

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Styles.css';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         I ADDED SOMETHING HERE FOR THE INITIAL COMMIT
-        </a>
       </header>
+      <body>
+        welcome
+      </body>
     </div>
   );
 }
