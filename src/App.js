ceeaddb249de8b3d5e60401ff184cebe7dbe7b54
Annalise Murphy <nali247@gmail.com>
2020-08-05 21:54:25 -0700

    images and seach rendering

--- src/App.js
+++ src/App.js
@@ -1,58 +1,56 @@
 import React from 'react';
 import './Styles.css';
 import request from 'superagent';
+// import SearchBar from './PokemonData.js';
 
-// need to create in put for userSearchParams
-// need to add a seach-Button
-// need to add/connect pokedex data to site
+// need to create in put for userSearchParams DONE
+// need to add a seach-Button DONE
+// need to add/connect pokedex data to site DONE
+
 // need to add drop down category as another seachParam option
 
-// must display Pokemon image: ex. url_image: "http://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png"
-// must display Pokemon name: ex. pokemon: "venusaur",
-// must display relevant data (attack, defence, anything you used in seach/sort) ex. type_1: / ability_hidden:
-
 class App extends React.Component {
-  state = {
+  state = { 
     search: '',
     isLoading: false,
     pokeState: []
   }
 
   handleClick = async () => {
-    this.setState({ isLoading: true})
+    this.setState({ isLoading: true })
     const data = await request.get(`https://alchemy-pokedex.herokuapp.com/api/pokedex?perPage=1000&pokemon=${this.state.search}`);
 
-    this.setState({
+    this.setState({ 
       pokeState: data.body.results,
       isLoading: false,
-    })
+     })
   }
 
-  handlePokemonType = (e) => {
+  handleDogType = (e) => {
     const type = e.target.value;
 
     this.setState({ filter: type })
   }
-
   render() {
     return (
-      <>
-        <header className={this.state.newColor}>
-
-          <input onChange={(e) => this.setState({ search: e.target.validationMessage })} />
-          <button onClick={this.handleClick}>Inspect Pokedex!</button>
-          {/* will put / */}
-          {/* {handleDropDown
-            this.state.isLoading
-
-          } */}
+        <header>
+          <input onChange={(e) => this.setState({ search: e.target.value})} />
+          <button onClick={this.handleClick}>Fetch Pokemon!</button>
+          {
+            this.state.isLoading 
+              ? <p>LOADING</p> 
+              //name type ability shape
+              : this.state.pokeState.map(poke => <p>Name: {poke.pokemon} Type: { poke.type_1 } Ability: { poke.ability_1 } Shape: { poke.shape } {<img className='poke-box' src={poke.url_image} alt='fart-face' />}</p>)
+          } 
         </header>
-
-        <body>welcome</body>
-      </>
     );
   }
 }
 
 export default App;
+
+
+//inputs into state and log out
+//change api call per the user choices saved int state
+//display everything from API call to display on the page
 
