1ebc4cd9b31bfc52212f1b2651bbe30c83180079
Annalise Murphy <nali247@gmail.com>
2020-08-07 14:27:23 -0700

    ListPage skeleton added

---
+++ src/ListPage/ListPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ListPage extends Component {
+    render() {
+        return (
+            <div>
+                List page here! 
+            </div>
+        )
+    }
+}
